824ef79d16c0f81c6024152a1eb6268f39833c53
philapheda <abrhamyishak222@gmail.com>
2025-08-02 14:43:14 +0300

    completed the frontend

--- Frontend/src/components/InactiveMessage.jsx
+++ Frontend/src/components/InactiveMessage.jsx
@@ -37,7 +37,7 @@ function InactiveMessage() {
     }
   };
    return (
-    <div className="flex flex-col gap-3 justify-center items-center">
+    <div className="flex flex-col gap-1 justify-center items-center">
 	<InTable datas = {message_data}/>
 	<button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800" onClick={()=>handleDelete()}>Clear</button>
     </div>
